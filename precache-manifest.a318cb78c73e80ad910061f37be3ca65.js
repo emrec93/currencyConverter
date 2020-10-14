88b073354f704426cee17d30610157973c723113
Emre <emrecakmak93@hotmail.co.uk>
2020-10-14 15:01:10 +0100

    Updates

---
+++ precache-manifest.a318cb78c73e80ad910061f37be3ca65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4ff7e1bf735e2da16a4561ce2d7caf",
+    "url": "/currencyConverter/index.html"
+  },
+  {
+    "revision": "a6cdd3bcf2a9520c5161",
+    "url": "/currencyConverter/static/css/main.b9e00895.chunk.css"
+  },
+  {
+    "revision": "7365d83fff62cb0af6f5",
+    "url": "/currencyConverter/static/js/2.edb56e9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currencyConverter/static/js/2.edb56e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6cdd3bcf2a9520c5161",
+    "url": "/currencyConverter/static/js/main.042a1db2.chunk.js"
+  },
+  {
+    "revision": "857dd21975e8d054f01e",
+    "url": "/currencyConverter/static/js/runtime-main.12c1242c.js"
+  }
+]);
